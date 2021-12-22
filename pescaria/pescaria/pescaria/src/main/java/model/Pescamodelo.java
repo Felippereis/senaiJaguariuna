cfa3e9de08e91d70473c7fda72eafc75401c1b83
Felipe Reis <felipepedreira2010@gmail.com>
2021-12-22 08:52:24 -0300

    Add files via upload

---
+++ pescaria/pescaria/pescaria/src/main/java/model/Pescamodelo.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Pescamodelo {
+	
+	//ATRIBUTOS
+	private int Id;
+	private String Cidade;
+	private String Quantidade;
+	
+	// gets e sets
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getCidade() {
+		return Cidade;
+	}
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+	public String getQuantidade() {
+		return Quantidade;
+	}
+	public void setQuantidade(String quantidade) {
+		Quantidade = quantidade;
+	}
+
+}
