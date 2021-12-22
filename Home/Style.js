56cca2c82dcbfd9cefed266acb4c13b0c856b429
Felipe Reis <felipepedreira2010@gmail.com>
2021-12-22 08:58:12 -0300

    Add files via upload

---
+++ Home/Style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+// gerando o style para esterilização
+export default StyleSheet.create(
+{
+    Input: {
+        flex:1,
+        alignItems:"center",
+        width:300
+    },
+texto: {
+    fontSize: 30,
+    color: 'red',     
+  },
+})
