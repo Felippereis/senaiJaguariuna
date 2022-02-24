5e61c5d2058b2aeab9e68349566cd1930a1b9468
Felipe Reis <felipepedreira2010@gmail.com>
2022-02-24 11:29:34 -0300

    Add files via upload

---
+++ mobile/mobile/pages/cadastro/login/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import { View, TextInput} from 'react-native';
+
+
+export default function Login({ navigation }) {
+
+
+
+return (
+    <View>        
+        
+    </View>
+);
+}
